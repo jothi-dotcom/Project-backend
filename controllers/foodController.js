718bd43d9ababd468a5af2c53ede52bbc0f7c2c7
Marijothi Marijothi <jothi15121999@gmail.com>
2025-09-30 22:35:06 +0530

    FOOD

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,55 +1,56 @@
 const Food = require("../models/foodModel");
 
-const createFood = async (req, res) => {
-    try {
-        const food = await Food.create(req.body);
-        res.status(201).json({ message: "food created", food })
-    } catch (err) {
-        res.status(500).json({ message: err.message })
+const createFood = async (req,res) =>{
+    try{
+        const food =await Food.create(req.body);
+        res.status(201).json({message:"food created" ,food})
+    }catch(err){
+        res.status(500).json({message: err.message})
     }
 };
 
-const getfoodByhotel = async (req, res) => {
-    try {
-        const getfood = await Food.find({ hotel: req.params.hotelID })
-        res.status(200).json({ getfood });
-    } catch (err) {
-        res.status(500).json({ message: err.message })
+const getfoodByhotel = async(req,res) =>{
+    try{
+        const getfood = await Food.find({hotel :req.params.hotelID})
+        res.status(200).json({getfood});
+    }catch(err){
+        res.status(500).json({message: err.message})
     }
 };
 const getFoodById = async (req, res) => {
     try {
-        const food = await Food.findById(req.params.id);
+        const food = await Food.findById(req.params.id).populate("hotel", "name");
         if (!food) return res.status(404).json({ message: "Food not found" });
+
         res.json({
             _id: food._id,
             name: food.name,
             price: food.price,
             image: food.image,
+            hotelName: food.hotel?.name || "Unknown",
         });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 };
-
-const updateFood = async (req, res) => {
-    try {
-        const updatedfood = await Food.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.status(200).json({ message: "Update successfully", updatedfood });
-    } catch (err) {
-        res.status(500).json({ message: "server error: Network connection Failed" });
+const updateFood = async(req,res) => {
+    try{
+        const updatedfood = await Food.findByIdAndUpdate(req.params.id ,req.body ,{new:true});
+        res.status(200).json({message:"Update successfully" ,updatedfood});
+    }catch(err){
+        res.status(500).json({message:"server error: Network connection Failed"});
     }
 };
 
-const deleteFood = async (req, res) => {
-    try {
-        const deletedfood = await Food.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: "Food deleted", deletedfood });
+const deleteFood = async(req,res) =>{
+    try{
+        const deletedfood = await Food.findByIdAndDelete(req.params.id );
+        res.status(200).json({message:"Food deleted" ,deletedfood});
 
-    } catch {
-        res.status(500).json({ message: "server error: Network connection failed" });
+    }catch{
+        res.status(500).json({message:"server error: Network connection failed"});
     }
 }
 
-module.exports = { createFood, getfoodByhotel, getFoodById, updateFood, deleteFood };
+module.exports ={createFood ,getfoodByhotel,getFoodById ,updateFood ,deleteFood};
 
